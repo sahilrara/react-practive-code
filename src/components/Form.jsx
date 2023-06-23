51a15f16a61c44e3248ef4c2d6efc51b691f3ed6
sahilrara <shashiparkashrara@gmail.com>
2023-06-23 10:34:58 +0530

    syllbus done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,31 +1,49 @@
-import React, { useRef } from "react";
-import emailjs from "@emailjs/browser";
+// import React, { useRef } from "react";
+// import emailjs from "@emailjs/browser";
+
+// const Form = () => {
+//   const form = useRef();
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs.sendForm(
+//       "service_ycz4jdo",
+//       "template_g0u4hnl",
+//       form.current,
+//       "fnrzC6sB0B51hp0PJ"
+//     );
+//   };
+//   return (
+//     <div>
+//       {" "}
+//       <form ref={form} onSubmit={sendEmail}>
+//         <label>Name</label>
+//         <input type="text" name="user_name" />
+//         <label>Email</label>
+//         <input type="email" name="user_email" />
+//         <label>Message</label>
+//         <textarea name="message" />
+//         <input type="submit" value="Send" />
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Form;
+
+import React, { useState } from "react";
 
 const Form = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm(
-      "service_ycz4jdo",
-      "template_g0u4hnl",
-      form.current,
-      "fnrzC6sB0B51hp0PJ"
-    );
-  };
+  const [name, setName] = useState("");
+  console.log(name);
   return (
     <div>
-      {" "}
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
     </div>
   );
 };
